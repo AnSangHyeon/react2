9fc330db3b5a65f4b40f406a24a4bb2eff78290d
AnSangHyeon <ash040906@naver.com>
2025-09-24 13:46:28 +0900

    마지막 커밋

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,6 +9,13 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <header>== header ==</header>
+        <nav>
+          <Link href="/">home</Link> | 
+          <Link href="/blog">blog</Link> | 
+          <Link href="/blog2">blog2</Link> | 
+          <Link href="/blog3">blog3</Link> |
+          <Link href="/product">product</Link>
+        </nav>
         {children}
         <footer>== footer ==</footer>
       </body>
