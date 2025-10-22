87300e7be3f86c7be81f6bd8f1ea111da588e702
AnSangHyeon <ash040906@naver.com>
2025-10-22 11:41:55 +0900

    test

---
+++ my-app/src/app/components/theme-status.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useContext } from "react";
+import { ThemeContext } from "./theme-provider";
+
+export default function ThemeStatus() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  
+  return (
+    <div
+      style={{
+        display: 'inline-block',
+        margin: '0 0 0 12px'
+      }}
+    >
+      <button
+        onClick={toggleTheme}
+        aria-label="toggle theme"
+      >
+        Theme: {theme === 'dark' ? '🌙Dark' : '☀️Light'}
+      </button>
+    </div>
+  );
+}
